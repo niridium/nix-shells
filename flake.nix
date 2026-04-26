a770f54666ef025e862780cec0515c6c3da1e9e0
niridium <niridium@tuta.io>
2026-04-26 14:35:27 +0200

    feat(rust): use rustup instead of nixpkgs

--- flake.nix
+++ flake.nix
@@ -9,14 +9,26 @@
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
+      overrides = (builtins.fromTOML (builtins.readFile ./rust-toolchain.toml));
     in
     {
       devShells.${system} = with pkgs; {
         rust = mkShell {
-          packages = [
-            cargo
-            rustc
+          stictDeps = true;
+          nativeBuildInputs = [
+            rustup
+            rustPlatform.bindgenHook
           ];
+          buildInputs = [ ];
+          RUSTC_VERSION = overrides.toolchain.channel;
+          shellHook = ''
+            export PATH="''${CARGO_HOME:-~/.cargo}/bin":"$PATH";
+            export PATH="''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-${stdenv.hostPlatform.rust.rustcTarget}/bin":"$PATH"
+          '';
+          # packages = [
+          #   cargo
+          #   rustc
+          # ];
         };
         git = mkShell {
           packages = [
